0394f51b7fd6b39b7df038defeb1d565b1f210c7
Israel Mendoza <xander_irsl@hotmail.com>
2020-08-02 16:02:00 -0500

    Working with decorators as loggers

---
+++ Decorators5.py
@@ -0,0 +1,34 @@
+import pytz
+from functools import wraps
+from datetime import datetime
+
+
+# Using Decorators as loggers
+
+
+def function_logger(a_function):
+    @wraps(a_function)
+    def logger(*args, **kwargs):
+        run_datetime = datetime.now(pytz.UTC)
+        result = a_function(*args, **kwargs)
+        _args = list(args)
+        _kwargs = [f"'{key}'={value}" for key, value in kwargs.items()]
+        all_args = tuple(_args + _kwargs)
+        print(f"{run_datetime}: called '{a_function.__name__}{all_args}'")
+        return result
+
+    return logger
+
+
+@function_logger
+def func1(a, b, c=10):
+    pass
+
+
+@function_logger
+def func2():
+    pass
+
+
+func1(2, 5)
+func2()
