824e19359de82a66b807b2dbc6968228d4ae4ad3
Israel Mendoza <xander_irsl@hotmail.com>
2020-08-17 00:58:42 -0500

    Added comments and docs to the classes and functions in first memoization file

--- DecoratorsMemoization.py
+++ DecoratorsMemoization.py
@@ -20,30 +20,45 @@ def function_logger(a_function: FunctionType) -> FunctionType:
 
 @function_logger
 def fibonacci(n):
+    """A Fibonacci number generator, using recursion"""
     if n <= 2:
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+# Using the decorated function on a fibonacci recursive function,
+# which shows the need of a caching system (multiple repeted calculations generated).
 # print(fibonacci(12))
 
-# Caching calculated results in a dictionary
+
+# Implementing a dictionary as a chaching mechanism
 
 
-class Fib:
+class FibonacciWrapper:
+    """
+    A class intended to wrap a fibonacci function.
+    Implements a caching mechanism as a memoization system
+    """
+
     def __init__(self):
+        # Initialized some well-known values for the Fibonacci sequence
         self.cache = {1: 1, 2: 1}
 
     def __call__(self, n: int) -> int:
+        # Calculating the fibonacci result
+        # _only_ if it doen't exists in the self.cache dictionary
         if n not in self.cache:
             print(f"Calculating fib({n})")
             self.cache[n] = self(n - 1) + self(n - 2)
+        # Returning the cached result
         return self.cache[n]
 
 
-f1 = Fib()
+f1 = FibonacciWrapper()
 print(f1(10))
+print(type(f1))  # f1 is a FibonacciWrapper callable object
+# Accessing the caching dictionary
 print(f"Cache dictionary: {f1.cache}")
 
 
@@ -51,14 +66,23 @@ print(f"Cache dictionary: {f1.cache}")
 
 
 def fibonacci_recursive() -> FunctionType:
+    """
+    A decorator intended to wrap a fibonacci function.
+    Implements a caching mechanism as a memoization system
+    """
+    # Initialized some well-known values for the Fibonacci sequence
     _cache = {1: 1, 2: 1}
 
     def inner(n: int):
+        # Calculating the fibonacci result
+        # _only_ if it doen't exists in the _cache dictionary
         if n not in _cache:
             print(f"Calculating fibonacci({n})")
             _cache[n] = inner(n - 1) + inner(n - 2)
+        # Returning the cached result
         return _cache[n]
 
+    # Making the _cache dictionary available through an interface
     inner.cache = _cache
 
     return inner
@@ -66,4 +90,6 @@ def fibonacci_recursive() -> FunctionType:
 
 f2 = fibonacci_recursive()
 print(f2(10))
+print(type(f2))  # f1 is a function
+# Accessing the caching dictionary
 print(f"Cache dictionary: {f2.cache}")
