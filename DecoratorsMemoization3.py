730c7a8df011774925ef6aab364649d2658f0054
Israel Mendoza <xander_irsl@hotmail.com>
2020-08-17 23:53:36 -0500

    Added a description to the third memoization file, where I use the lru_cache decorator

--- DecoratorsMemoization3.py
+++ DecoratorsMemoization3.py
@@ -1,7 +1,10 @@
 from functools import lru_cache
 
+# lru_cache takes in the max_cache_value
+# It is nothing more than a memoization decorator
 
-@lru_cache(maxsize=8)
+
+@lru_cache(8)
 def fibonacci(n):
     print(f"Calculating {n}!")
     return 1 if n < 3 else fibonacci(n - 1) + fibonacci(n - 2)
