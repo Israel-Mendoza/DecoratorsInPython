e59b1ec922ffdad9c54c062be91ac19121057779
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-23 23:11:36 -0500

    Stack decorators breakdown

--- Decorators7.py
+++ Decorators7.py
@@ -11,9 +11,8 @@ from functools import wraps
 def decorator_1(a_func: FunctionType) -> FunctionType:
     @wraps(a_func)
     def inner(*args, **kwargs) -> Any:
-
-        result = a_func(*args, **kwargs)
         print("Running decorator 1!")
+        result = a_func(*args, **kwargs)
         return result
     return inner
 
@@ -21,7 +20,6 @@ def decorator_1(a_func: FunctionType) -> FunctionType:
 def decorator_2(a_func: FunctionType) -> FunctionType:
     @wraps(a_func)
     def inner(*args, **kwargs) -> Any:
-
         result = a_func(*args, **kwargs)
         print("Running decorator 2!")
         return result
@@ -41,11 +39,11 @@ simple_function_1 = decorator_2(simple_function_1)
 # Another way of decorating the function:
 simple_function_2 = decorator_2(decorator_1(simple_function))
 
-# Another way of decorating the function:
+# Another way of decorating the function (reversed than the previous ones):
 
 
-@decorator_2
 @decorator_1
+@decorator_2
 def simple_function_3(name: str) -> None:
     """Simple function that greets the passed name"""
     print(f"Hello, {name}!")
@@ -54,13 +52,13 @@ def simple_function_3(name: str) -> None:
 # Running all three decorated functions
 simple_function_1("Israel")
 # Running decorator 1!
-# Running decorator 2!
 # Hello, Israel!
+# Running decorator 2!
 simple_function_2("Israel")
 # Running decorator 1!
-# Running decorator 2!
 # Hello, Israel!
+# Running decorator 2!
 simple_function_3("Israel")
 # Running decorator 1!
-# Running decorator 2!
 # Hello, Israel!
+# Running decorator 2!
