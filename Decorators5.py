e48b5ae8284c47560aeacfe56f67d0f913f2700e
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-23 22:36:03 -0500

    Documenting logger decorator

--- Decorators5.py
+++ Decorators5.py
@@ -1,29 +1,33 @@
+"""Using Decorators as loggers"""
+
+
 import pytz
 from types import FunctionType
+from typing import Any
 from functools import wraps
 from datetime import datetime
 
 
-# Using Decorators as loggers
-
-
-def function_logger(a_function: FunctionType) -> FunctionType:
+def function_logger(a_func: FunctionType) -> FunctionType:
     """
     Decorator function. Returns a closure that, when called,
     will print the time the function was called and a function
     call representation.
     """
-
-    @wraps(a_function)
-    def logger(*args, **kwargs):
+    @wraps(a_func)
+    def logger(*args, **kwargs) -> Any:
+        # Capturing date and time object when wrapped function is called
         run_datetime = datetime.now(pytz.UTC)
-        result = a_function(*args, **kwargs)
+        # Running function and storing its return value
+        result = a_func(*args, **kwargs)
+        # Capturing args and kwargs in a tuple
         _args = list(args)
         _kwargs = [f"'{key}'={value}" for key, value in kwargs.items()]
         all_args = tuple(_args + _kwargs)
-        print(f"{run_datetime}: called '{a_function.__name__}{all_args}'")
+        # Printing logger info (to stdout but could be log server)
+        print(f"{run_datetime}: called '{a_func.__name__}{all_args}'")
         return result
-
+    # Returning decorator
     return logger
 
 
@@ -37,5 +41,5 @@ def func2():
     pass
 
 
-func1(2, 5)
-func2()
+func1(2, 5)  # 2020-09-24 03:31:29.801927+00:00: called 'func1(2, 5)'
+func2()  # 2020-09-24 03:31:29.801927+00:00: called 'func2()'
