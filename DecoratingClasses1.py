d4f7b6b71c68c6a9275b5211b609e2139d0a49ec
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-26 00:25:04 -0500

    Documented DecoratingClasses1.py

--- DecoratingClasses1.py
+++ DecoratingClasses1.py
@@ -1,39 +1,113 @@
+"""
+Decorating classes by "monkey patching" them
+by adding a new instance method to its namespace.
+"""
+
+
+from typing import Type
 from fractions import Fraction
 
 
-def add_instance_method(cls):
-    # Adds an instance method to the passed class (monkey patching)
-    cls.speak = lambda self, message: f'{self.__class__.__name__} says "{message}"'
+def add_instance_method(cls: Type) -> Type:
+    """
+    Sets a function/method to the passed class as a "speak"
+    attribute (monkey patching)
+    Returns the class for reassigment.
+    """
+    # If intended as an instance method, 
+    # remember to include the "self" argument
+    def speak(self, message: str) -> str:
+        """Function that could serve as a method"""
+        return f'{self.__class__.__name__} says "{message}"'
+    # Monkey patching the passed class
+    setattr(cls, "speak", speak)
+    # Returns the class
     return cls
 
 
 class Person:
-    def __init__(self, name, age):
+    """Simple representation of a person"""
+
+    def __init__(self, name: str, age: int) -> None:
+        """Initialized the Person instance"""
         self._name = name
         self._age = age
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """The Person's name"""
         return self._name
 
     @name.setter
-    def name(self, value):
+    def name(self, value: str) -> None:
         self._name = value
 
     @property
-    def age(self):
+    def age(self) -> int:
+        """The Person's age"""
         return self._age
 
     @age.setter
-    def age(self, value):
+    def age(self, value: int):
         self._age = value
 
 
-# Decorating the existant class
-Fraction = add_instance_method(Fraction)
-Person = add_instance_method(Person)
-f1 = Fraction(8 / 4)
+"""Working with a custom class"""
+
+# Checking Person's namespace
+print(vars(Person)) # {... __init__, "name", "age" ...}
+
+# Confirming that the attribute doesn't exist yet
+print(hasattr(Person, "speak")) # False
+
+# Creating a Person instance
 p1 = Person("Israel Mendoza", 28)
 
-print(f1.speak("soy una puta fracción"))
-print(p1.speak("no soy nada"))
+# Trying the "speak" attribute
+try:
+    print(p1.speak("Je pense, donc je suis."))
+except AttributeError as ex:
+    print(ex) # 'Person' object has no attribute 'speak'
+
+# Decorating the class (monkey patching it)
+Person = add_instance_method(Person)
+
+# Checking Person's namespace
+print(vars(Person)) # {... __init__, "name", "age", "speak"...}
+
+# Confirming that the attribute exists now
+print(hasattr(Person, "speak")) # True
+
+# Trying the "speak" attribute
+try:
+    print(p1.speak("Je pense, donc je suis.")) # Person says "Je pense, donc je suis."
+except AttributeError as ex:
+    print(ex)
+
+
+"""Working with an existing class"""
+
+
+# Confirming that the attribute doesn't exist yet
+print(hasattr(Fraction, "speak")) # False
+
+# Creating a Fraction instance
+f1 = Fraction(1 / 3)
+
+# Trying the "speak" attribute
+try:
+    print(f1.speak("To be or not to be?"))
+except AttributeError as ex:
+    print(ex) # 'Fraction' object has no attribute 'speak'
+
+# Decorating the class (monkey patching it)
+Fraction = add_instance_method(Fraction)
+
+# Confirming that the attribute exists now
+print(hasattr(Fraction, "speak")) # True
+
+# Trying the "speak" attribute
+try:
+    print(f1.speak("To be or not to be?")) # Fraction says "To be or not to be?"
+except AttributeError as ex:
+    print(ex)
