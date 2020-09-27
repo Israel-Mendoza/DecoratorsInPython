3f3980060f25eeb2ce79326dee8b194c1a30adfd
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-27 16:12:03 -0500

    Documented DecoratorsMemoization.py

--- DecoratorsMemoization.py
+++ DecoratorsMemoization.py
@@ -1,23 +1,41 @@
+"""Introducing memoization"""
+
+
 import pytz
+from typing import Any
 from datetime import datetime
 from types import FunctionType
 from functools import wraps
 
 
 def function_logger(a_function: FunctionType) -> FunctionType:
+    """Function decorator"""
     @wraps(a_function)
-    def logger(*args, **kwargs):
+    def logger(*args, **kwargs) -> Any:
+        """
+        Wrapper function.
+        Prints information about the time and arguments the
+        wrapped function is called.
+        """
+        # Capturing time the function is called
         run_datetime = datetime.now(pytz.UTC)
+        # Storing the result of the called wrapped function
         result = a_function(*args, **kwargs)
-        _args = list(args)
+        # Capturing the arguments in a tuple
+        _args = [str(arg) for arg in args]
         _kwargs = [f"'{key}'={value}" for key, value in kwargs.items()]
-        all_args = tuple(_args + _kwargs)
+        _args = ", ".join(_args)
+        _kwargs = ", ".join(_kwargs)
+        all_args = _args + _kwargs
+        # Printing the information
         print(f"{run_datetime}: called '{a_function.__name__}({all_args})'")
+        # Returning the wrapped function's result
         return result
-
+    # Returning the wrapped function
     return logger
 
 
+# Using the decorated function on a fibonacci recursive function
 @function_logger
 def fibonacci(n):
     """A Fibonacci number generator, using recursion"""
@@ -27,50 +45,104 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-# Using the decorated function on a fibonacci recursive function,
-# which shows the need of a caching system (multiple repeted calculations generated).
-# print(fibonacci(12))
+"""
+Calling a decorated recursive function. 
+Calculations are duplicated, which can be expensive.
+"""
+
+# Notice how fibonacci(2) gets calculated up to 4 times
+# when calling fibonacci(6)!!!
+# print(fibonacci(6))
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(2)' 
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(1)'
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(3)'
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(2)' -> Already calculated
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(4)'
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(2)' -> Already calculated (2)
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(1)' -> Already calculated
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(3)' -> Already calculated
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(5)'
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(2)' -> Already calculated (3)
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(1)' -> Already calculated (2)
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(3)' -> Already calculated (2)
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(2)' -> Already calculated (4)
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(4)' -> Already calculated
+# 2020-09-27 20:40:40.362964+00:00: called 'fibonacci(6)'
+# 8
 
 
-# Implementing a dictionary as a chaching mechanism
+"""
+Implementing dictionaries in a class as a chaching 
+mechanisms to prevent inecessary calculations.
+"""
 
 
-class FibonacciWrapper:
+class FibonacciClass:
     """
-    A class intended to wrap a fibonacci function.
+    A class that contains a fibonacci calculator.
+    Class instance is callable, which is the fibonacci calculator.
     Implements a caching mechanism as a memoization system
+    to avoid duplicated calculations.
     """
-
     def __init__(self):
-        # Initialized some well-known values for the Fibonacci sequence
+        """
+        Initializing the caching mechanism containing
+        known values for fibonacci(1) and fibonacci(2).
+        """
         self.cache = {1: 1, 2: 1}
 
     def __call__(self, n: int) -> int:
-        # Calculating the fibonacci result
-        # _only_ if it doen't exists in the self.cache dictionary
+        """
+        When the class instance is called, it calculates
+        fibonnaci(n).
+        First, it looks for the existing cached value;
+        if found, returned, if not, calculates it and
+        stores it in the cache dictionary.
+        """
         if n not in self.cache:
+            # Prints the object calling only wheb
+            # values are calculated
             print(f"Calculating fib({n})")
             self.cache[n] = self(n - 1) + self(n - 2)
         # Returning the cached result
         return self.cache[n]
 
 
-f1 = FibonacciWrapper()
+# Creating a FibonacciClass instance
+f1 = FibonacciClass()
+
+# Notice how there are no duplicated calculations
 print(f1(10))
-print(type(f1))  # f1 is a FibonacciWrapper callable object
+# Calculating fib(10)
+# Calculating fib(9)
+# Calculating fib(8)
+# Calculating fib(7)
+# Calculating fib(6)
+# Calculating fib(5)
+# Calculating fib(4)
+# Calculating fib(3)
+
+print(type(f1))  # <class '__main__.FibonacciClass'>
+
 # Accessing the caching dictionary
-print(f"Cache dictionary: {f1.cache}")
+print(f1.cache) 
+# {1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8, 7: 13, 8: 21, 9: 34, 10: 55}
 
 
-# Recreating the previous examples with closures
+"""
+Implementing dictionaries in a class as a chaching 
+mechanisms to prevent inecessary calculations.
+"""
 
 
-def fibonacci_wrapper() -> FunctionType:
+def fibonacci_function() -> FunctionType:
     """
-    A decorator intended to wrap a fibonacci function.
-    Implements a caching mechanism as a memoization system
+    Returns a fibonacci calculator function.
+    Returned function implements a caching dictionary 
+    as a memoization mechanism.
     """
-    # Initialized some well-known values for the Fibonacci sequence
+    #Initializing the caching mechanism containing
+    # known values for fibonacci(1) and fibonacci(2).
     _cache = {1: 1, 2: 1}
 
     def inner(n: int):
@@ -88,8 +160,23 @@ def fibonacci_wrapper() -> FunctionType:
     return inner
 
 
-f2 = fibonacci_wrapper()
+# Storing the returned function from 
+# "fibonacci_function" to f2
+f2 = fibonacci_function()
+
+# Notice how there are no duplicated calculations
 print(f2(10))
-print(type(f2))  # f1 is a function
+# Calculating fibonacci(10)
+# Calculating fibonacci(9)
+# Calculating fibonacci(8)
+# Calculating fibonacci(7)
+# Calculating fibonacci(6)
+# Calculating fibonacci(5)
+# Calculating fibonacci(4)
+# Calculating fibonacci(3)
+
+print(type(f2))  # <class 'function'>
+
 # Accessing the caching dictionary
-print(f"Cache dictionary: {f2.cache}")
+print(f2.cache)
+# {1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8, 7: 13, 8: 21, 9: 34, 10: 55}
