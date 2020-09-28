714bed94a4feb4456b8b3dc6be9ef2ebf77ac5b9
Israel Mendoza <xander_irsl@hotmail.com>
2020-09-27 20:17:22 -0500

    Documented DecoratorsMemoization2.py

--- DecoratorsMemoization2.py
+++ DecoratorsMemoization2.py
@@ -1,18 +1,24 @@
+"""Memoizing any function using a decorator"""
+
+
 from functools import wraps
 from types import FunctionType
 
 
 def memoize(a_function: FunctionType) -> FunctionType:
-    """A wrapper memoizationfunction for a function 
-        that takes only 1 argument
+    """
+    A wrapper memoization function for a 
+    function that takes only 1 argument.
     """
     # Implementing the cache dictionary
     _cache = {}
 
     @wraps(a_function)
     def inner(n: int):
-        # Calculating the return value only if
-        # it doesn't exist in the _cache dictionary
+        """
+        Calculating the return value only if
+        it doesn't exist in the _cache dictionary
+        """
         if n not in _cache:
             _cache[n] = a_function(n)
         return _cache[n]
@@ -26,8 +32,9 @@ def memoize(a_function: FunctionType) -> FunctionType:
 
 @memoize
 def fibonacci(n: int) -> int:
-    """A function that returns the passed
-        Fibonacci sequence's index using recursion
+    """
+    A function that returns the passed
+    Fibonacci sequence's index using recursion
     """
     if n < 3:
         return 1
@@ -35,8 +42,31 @@ def fibonacci(n: int) -> int:
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-print(f"Fibonacci(10): {fibonacci(10)}\n")
-print(f"Function's cache: {fibonacci.cache}\n")
-print(f"Fibonacci(10) again (from cache!!!): {fibonacci(10)}\n")
+print(fibonacci(10))
+# Calculating 10!
+# Calculating 9!
+# Calculating 8!
+# Calculating 7!
+# Calculating 6!
+# Calculating 5!
+# Calculating 4!
+# Calculating 3!
+
+print(fibonacci.cache)
+# {2: 1, 1: 1, 3: 2, 4: 3, 5: 5, 6: 8, 7: 13, 8: 21, 9: 34, 10: 55}
+
+# Calculating fibonacci(10) again
+# Notice how value is taken from caching dictionary
+print(fibonacci(10))
+# 55
+
 print(f"Fibonacci(8) (from cache!!!): {fibonacci(8)}\n")
+# Fibonacci(8) (from cache!!!): 21
+
 print(f"Fibonacci(15): {fibonacci(15)}\n")
+# Calculating 15!
+# Calculating 14!
+# Calculating 13!
+# Calculating 12!
+# Calculating 11!
+# Fibonacci(15): 610
